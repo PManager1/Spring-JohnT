b845724af8d663c402acb45c3853c30046995e4c
Jay <jpca999@gmail.com>
2023-05-06 14:34:49 -0400

    23 done

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -56,6 +56,11 @@ public class BootstrapData implements CommandLineRunner {
         ericSaved.getBooks().add(dddSaved);
         rodSaved.getBooks().add(noEJBSaved);
 
+        dddSaved.getAuthors().add(ericSaved);
+        noEJBSaved.getAuthors().add(rodSaved);
+
+
+
         Publisher publisher = new Publisher();
         publisher.setPublisherName("My Publisher");
         publisher.setAddress("123 Main");
